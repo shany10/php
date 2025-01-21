3671ad21a85939c0a383bfd71c1f4f0603aabb86
aminnairi <18418459+aminnairi@users.noreply.github.com>
2025-01-21 12:49:07 +0100

    initial commit

---
+++ db/migrations/000001_initial.php
@@ -0,0 +1,16 @@
+<?php
+
+// PHP Data Object
+$databaseConnection = new PDO(
+  "mysql:host=mariadb;dbname=database",
+  "user",
+  "password"
+);
+
+$databaseConnection->query("
+  CREATE TABLE users(
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    email VARCHAR(30) NOT NULL UNIQUE,
+    password CHAR(60) NOT NULL
+  );
+");
