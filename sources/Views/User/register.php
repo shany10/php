5e052d3f31fb279a6bec85ac6143c13188aac720
sylvain <sylvainanton77@gmail.com>
2025-02-19 01:46:46 +0100

    ajout de css

--- sources/Views/User/register.php
+++ sources/Views/User/register.php
@@ -1,42 +1,97 @@
-<?php if (!empty($errors)): ?>
-    <div class="error-messages">
-        <ul>
+<div class="container">
+    <h2 class="text-center">Créer un compte</h2>
+
+    <?php if (!empty($errors)): ?>
+        <ul class="error-list">
             <?php foreach ($errors as $error): ?>
-                <li style="color: red;"><?= htmlspecialchars($error, ENT_QUOTES, 'UTF-8') ?></li>
+                <li><?= htmlspecialchars($error) ?></li>
             <?php endforeach; ?>
         </ul>
-    </div>
-<?php endif; ?>
+        <?php unset($_SESSION['errors']); ?>
+    <?php endif; ?>
 
+    <form action="/register" method="POST" class="login-form">
+        <div class="input-wrapper">
+            <i class="fas fa-envelope"></i>
+            <input
+                type="email"
+                name="email"
+                id="email"
+                class="input-field"
+                required
+                placeholder="Votre email"
+                value="<?= htmlspecialchars($_POST['email'] ?? '') ?>">
+        </div>
 
-<form action="/register" method="POST">
-    <input
-        type="email"
-        name="email"
-        placeholder="Votre email"
-        <?php echo (!empty($_POST['email']) ? "value='" . $_POST['email'] . "'" : "") ?>
-        required><br>
-    <input
-        type="text"
-        name="firstname"
-        placeholder="Firstname..."
-        <?php echo (!empty($_POST['firstname']) ? "value='" . $_POST['firstname'] . "'" : "") ?>
-        required><br>
-    <input
-        type="text"
-        name="lastname"
-        placeholder="Lastname..."
-        <?php echo (!empty($_POST['lastname']) ? "value='" . $_POST['lastname'] . "'" : "") ?>
-        required><br>
-    <input
-        type="text"
-        name="country"
-        placeholder="Country..."
-        <?php echo (!empty($_POST['country']) ? "value='" . $_POST['country'] . "'" : "") ?>
-        required><br>
-    <input type="password" name="password" placeholder="Votre mot de passe" required minlength="6"><br>
-    <input type="password" name="passwordConfirm" placeholder="Confirmation" required minlength="6"><br>
-    <input type="submit" value="S'inscrire"><br>
-</form>
-<p>Déjà un compte ? <a href="/login">Connectez-vous ici</a>.</p>
-<p><a href="/">Accueil</a>.</p>
+        <div class="input-wrapper">
+            <i class="fas fa-user"></i>
+            <input
+                type="text"
+                name="firstname"
+                id="firstname"
+                class="input-field"
+                required
+                placeholder="Prénom"
+                value="<?= htmlspecialchars($_POST['firstname'] ?? '') ?>">
+        </div>
+
+        <div class="input-wrapper">
+            <i class="fas fa-user"></i>
+            <input
+                type="text"
+                name="lastname"
+                id="lastname"
+                class="input-field"
+                required
+                placeholder="Nom"
+                value="<?= htmlspecialchars($_POST['lastname'] ?? '') ?>">
+        </div>
+
+        <div class="input-wrapper">
+            <i class="fas fa-globe"></i>
+            <input
+                type="text"
+                name="country"
+                id="country"
+                class="input-field"
+                required
+                placeholder="Pays"
+                value="<?= htmlspecialchars($_POST['country'] ?? '') ?>">
+        </div>
+
+        <div class="input-wrapper">
+            <i class="fas fa-lock"></i>
+            <input
+                type="password"
+                name="password"
+                id="password"
+                class="input-field"
+                required
+                placeholder="Mot de passe"
+                minlength="6">
+        </div>
+
+        <div class="input-wrapper">
+            <i class="fas fa-lock"></i>
+            <input
+                type="password"
+                name="passwordConfirm"
+                id="passwordConfirm"
+                class="input-field"
+                required
+                placeholder="Confirmez le mot de passe"
+                minlength="6">
+        </div>
+
+        <button type="submit" class="button">S'inscrire</button>
+    </form>
+
+    <p class="text-center">
+        Déjà un compte ?
+        <a href="/login" class="link">Connectez-vous ici</a>.
+    </p><br>
+
+    <p class="text-center">
+        <a href="/" class="link">Accueil</a>
+    </p>
+</div>
